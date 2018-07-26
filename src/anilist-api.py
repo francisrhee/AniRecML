c62077553dbcb7fd0b0049a3732ba47342b9f5c1
Francis <francisrhee@hotmail.com>
2018-07-25 22:09:52 -0400

    Combining data for input

--- src/anilist-api.py
+++ src/anilist-api.py
@@ -1,6 +1,8 @@
 import requests
 import json
 import re
+import numpy as np
+from helper_functions import encodeOneHot
 
 ''' TODO:
 1. Find a query that gets a list of all animes on user's list, and their score/features
@@ -11,8 +13,6 @@ import re
 6. Plot learning curves from different account examples to see if more data would help (error vs training set size for both training and cv)
 7. Port to a website using Django?
 '''
-
-
 
 def queryFeatures():
     query = '''
@@ -54,12 +54,11 @@ def queryFeatures():
     responseJSON = json.loads(response.text)
 
     # Extract relevant features
-    fullGenreList = responseJSON['data']['GenreCollection']
+    genreListFull = responseJSON['data']['GenreCollection']
     lists = responseJSON['data']['MediaListCollection']['lists']
     lists = [lists[x] for x in range(len(lists)) if bool(re.search('Completed|Dropped', lists[x]['name']))]
 
     scores = [lists[l]['entries'][x]['score'] for l in range(len(lists)) for x in range(len(lists[l]['entries']))]
-    media = [lists[l]['entries'][x]['media'] for l in range(len(lists)) for x in range(len(lists[l]['entries']))]
     numEpisodes = [lists[l]['entries'][x]['media']['episodes'] for l in range(len(lists)) for x in
                    range(len(lists[l]['entries']))]
     avgEpDuration = [lists[l]['entries'][x]['media']['duration'] for l in range(len(lists)) for x in
@@ -73,8 +72,7 @@ def queryFeatures():
     for i in range(len(studios)):
         studiosFormatted.append([studios[i][s]['name'] for s in range(len(studios[i]))])
 
-    studioListFull = getStudioListFull()
-
+    return scores, numEpisodes, avgEpDuration, genres, studiosFormatted, genreListFull
 
 
 # List of all possible studios takes a little more effort to get
@@ -109,7 +107,19 @@ def getStudioListFull():
 
     return studioList
 
+def combineData(scores, numEpisodes, avgEpDuration, genres, studiosFormatted, genreListFull, studioListFull):
+
+    # Encode genres and studios to one-hot and combine data
+    genresOneHot = encodeOneHot(genreListFull, genres)
+    studiosOneHot = encodeOneHot(studioListFull, studiosFormatted)
+
+    dataList = np.vstack((scores, numEpisodes, avgEpDuration)).T
+    dataList = list(np.hstack((dataList, genresOneHot, studiosOneHot)))
+
+    return dataList
 
 
 url = 'https://graphql.anilist.co'
-queryFeatures()
+scores, numEpisodes, avgEpDuration, genres, studiosFormatted, genreListFull = queryFeatures()
+studioListFull = getStudioListFull()
+inputData = combineData(scores, numEpisodes, avgEpDuration, genres, studiosFormatted, genreListFull, studioListFull)
