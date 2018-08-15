7f5fe30f1b617f07ec963894b0f2472c298b8c34
Francis <francisrhee@hotmail.com>
2018-08-15 18:14:22 -0400

    Previous ongoing series code didn't function correctly - trying to fix, may need to look at test/train sets because predictions is limited

--- src/collabfilt/train.py
+++ src/collabfilt/train.py
@@ -8,16 +8,23 @@ from surprise import NormalPredictor
 from surprise.model_selection import cross_validate
 from surprise.model_selection import train_test_split
 from surprise.model_selection import GridSearchCV
-
+import pandas as pd
 from collabfilt.input import getData
 
 
 def get_top_n(df, user, predictions, n=10):
     # First map the predictions to each user.
     top_n = defaultdict(list)
+    top_n_ongoing = defaultdict(list)
     for uid, iid, true_r, est, _ in predictions:
+        top_n[uid].append((iid, est))
+        top_n_ongoing[uid] = []
+
+    for iid, est in top_n[user]: # predictions is limited for each user
         if (df.loc[df['MediaTitle'] == iid]['CurrentlyAiring'].iloc[0] == True):
-            top_n[uid].append((iid, est))
+            top_n_ongoing.append((iid, est))
+
+    # TODO: Not many people have currently airing shows rated. Need more data for this.
 
     # Then sort the predictions for each user and retrieve the k highest ones.
     for uid, user_ratings in top_n.items():
@@ -28,9 +35,14 @@ def get_top_n(df, user, predictions, n=10):
 
 def train(user):
     # Get data
-    df = getData(user)
+    # df = getData(user)
+    df = pd.read_csv("C:/Users/francis/PycharmProjects/AniRecML/src/collabfilt/data.csv")
     reader = Reader(rating_scale=(1, 10))
     data = Dataset.load_from_df(df[['UserName', 'MediaTitle', 'Score']], reader=reader)
+
+
+
+
 
     # # Tune params
     # param_grid = {'n_epochs': [5, 10], 'lr_all': [0.002, 0.005],
@@ -43,7 +55,7 @@ def train(user):
     # algo.fit(data.build_full_trainset())
     # results_df = pd.DataFrame.from_dict(gs.cv_results)
 
-    trainset, testset = train_test_split(data, test_size=.25)
+    trainset, testset = train_test_split(data, test_size=.25) # Predictions doesn't predict everything. Try not splitting train and test sets?
 
     # Train
     algo = SVD()
@@ -55,7 +67,7 @@ def train(user):
     # Print the recommended items for each user
     for i in range(len(top_n)):
         print(top_n[i][0])
-
+    print("End.")
     # Metrics
     # accuracy.rmse(predictions)
     # cross_validate(NormalPredictor(), data, cv=2, verbose=True)
